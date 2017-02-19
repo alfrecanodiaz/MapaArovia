a80c640c42d475034cf2350993ff7f7f842eee7c
Alfredo Cano <alfrecanodiaz@gmail.com>
2017-02-19 10:27:34 -0300

    Infowindow con tabla y pestañas

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
     <link href="bootstrap/css/simple-sidebar.css" rel="stylesheet" />
     <!-- styles -->
     <link href="css/styles.css" rel="stylesheet" />
-	<!-- Map -->
-<!--	  <link rel="stylesheet" href="css/cartodb/cartodb.css" />-->
-	  <link rel="stylesheet" href="http://geo.stp.gov.py/dist/themes/css/cartodb.css" />
-	  <link rel="stylesheet" href="css/leaflet/MarkerCluster.Default.css" />
+  <!-- Map -->
+<!--    <link rel="stylesheet" href="css/cartodb/cartodb.css" />-->
+    <link rel="stylesheet" href="http://geo.stp.gov.py/dist/themes/css/cartodb.css" />
+    <link rel="stylesheet" href="css/leaflet/MarkerCluster.Default.css" />
       <link rel="stylesheet" href="css/leaflet/leaflet.awesome-markers.css">
       <link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
       <link rel="stylesheet" href="css/arovia.css" />
@@ -26,36 +26,36 @@
   </head>
   <body>
 
-    <?php include 'partials/header.php'?>
+    <?php include 'partials/header.php' ?>
 
     <div class="page-content">
 
-    	<div class="row parent-row">
+      <div class="row parent-row">
 
-		  <div class="col-md-12">
-		  	<div class="row">
-		  		<div class="col-md-12 a-map-container">
-		  			<div class="content-box-large parent-map">
-		  				<!--<div class="panel-heading">-->
-							<!--<div class="panel-title">Contenedor del mapa</div>-->
+      <div class="col-md-12">
+        <div class="row">
+          <div class="col-md-12 a-map-container">
+            <div class="content-box-large parent-map">
+              <!--<div class="panel-heading">-->
+              <!--<div class="panel-title">Contenedor del mapa</div>-->
 
-							<!--<div class="panel-options">-->
-								<!--<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>-->
-							<!--</div>-->
-						<!--</div>-->
-		  				<div id="map" class="panel-body sidebar-map">
+              <!--<div class="panel-options">-->
+                <!--<a href="#" data-rel="collapse"><i class="glyphicon glyphicon-refresh"></i></a>-->
+              <!--</div>-->
+            <!--</div>-->
+              <div id="map" class="panel-body sidebar-map">
 
 
-		  				</div>
-		  			</div>
-		  		</div>
-		  </div>
-		</div>
+              </div>
+            </div>
+          </div>
+      </div>
+    </div>
     </div>
 
-    <?php include 'partials/footer.php'?>
+    <?php include 'partials/footer.php' ?>
 
-    <?php include 'partials/infowindow/right-modal.php'?>
+    <?php include 'partials/infowindow/right-modal.php' ?>
 
     </div>
 
@@ -64,17 +64,17 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="js/jquery.waitforimages.js"></script>
-	<!-- Map -->
-<!--	<script src="js/cartodb/cartodb.js"></script>-->
-	<script src="http://geo.stp.gov.py/cartodb.js/v3/3.15/cartodb.js"></script>
-	<script src="js/leaflet/leaflet.markercluster.js"></script>
+  <!-- Map -->
+<!--  <script src="js/cartodb/cartodb.js"></script>-->
+  <script src="http://geo.stp.gov.py/cartodb.js/v3/3.15/cartodb.js"></script>
+  <script src="js/leaflet/leaflet.markercluster.js"></script>
     <script src="js/leaflet/leaflet.awesome-markers.min.js"></script>
-	<!-- Custom -->
+  <!-- Custom -->
     <script src="js/custom.js"></script>
 
-    <?php include 'partials/functions/main.php'?>
+    <?php include 'partials/functions/main.php' ?>
 
-  	<script>
+    <script>
 
         function query() {
 
@@ -121,7 +121,7 @@
 
         window.onload = main;
 
-	</script>
+  </script>
 
     <!-- Custom JS Map -->
     <!--<script>
--- partials/infowindow/right-modal.php
+++ partials/infowindow/right-modal.php
@@ -5,12 +5,26 @@
 
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <div id="modal-map-title">
-
-                </div>
+                <div id="modal-map-title"></div>
+                <h4 class="modal-title"></h4>
             </div>
 
             <div id="modal-map-content" class="modal-body">
+
+                <div class="tabbable"> <!-- Only required for left/right tabs -->
+                    <ul id="nav-modal-content" class="nav nav-tabs">
+                        <li id="option-tab-1" class="active"><a href="#tab-content-1" data-toggle="tab">General</a></li>
+                        <li id="option-tab-2"><a href="#tab-content-2" data-toggle="tab">Detalles</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="tab-content-1">
+
+                        </div>
+                        <div class="tab-pane" id="tab-content-2">
+
+                        </div>
+                    </div>
+                </div>
 
             </div>
 
